437c4c8031ce67b075570573f370b25548d7391b
Artur Miglio <artur@antiloop.io>
2017-04-26 18:51:57 +0200

    fix sass task

--- app/templates/gulp/tasks/sass.js
+++ app/templates/gulp/tasks/sass.js
@@ -3,20 +3,28 @@
 var gulp = require('gulp'),
     config = require('../config.json'),
     sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
     gulpif = require('gulp-if'),
-    autoprefixer = require('gulp-autoprefixer'),
+    autoprefixer = require('autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
     browserSync = require('browser-sync'),
     handleErrors = require('../utils/handle-errors'),
     filter = require('gulp-filter');
 
 gulp.task('sass', function() {
+
+    var plugins = [
+        autoprefixer({browsers: ['last 2 versions','ie >= 9','iOS 8']})
+    ];
+
     return gulp.src(config.sass + '/**/*.scss')
         .pipe(gulpif(global.isWatching, sourcemaps.init()))
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions','ie >= 9','iOS 8']
+        .pipe(sass({
+            includePaths: [
+                'node_modules/'
+            ]
         }))
+        .pipe(postcss(plugins))
         .pipe(gulpif(global.isWatching, sourcemaps.write('./')))
         .on('error', handleErrors)
         .pipe(gulp.dest(config.css))
