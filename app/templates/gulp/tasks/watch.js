ca144b16ca538c80f2bc8af624dffc3517c06c7b
Artur Miglio <artur@antiloop.io>
2017-04-26 19:56:22 +0200

    fix watch task

--- app/templates/gulp/tasks/watch.js
+++ app/templates/gulp/tasks/watch.js
@@ -23,20 +23,6 @@ if (argv.lint) {
 
 } else {
 
-    gulp.task('watch', ['browserify:vendor', 'browserify:app'], function() {
-        gulp.watch(config.dev + 'index.html', browserSync.reload);
-        gulp.watch(config.dev + 'templates/**/*.html', ['browserify:app']);
-        gulp.watch(config.sprites + '/*-2x/*.png', ['resize-sprites']);
-        gulp.watch(config.fonts + '/icomoon/**/*.*', ['icons']);
-        gulp.watch(config.sass + '/**/*.scss', ['sass']);
-        gulp.watch([
-            config.js + '/**/*.{js,jsx}',
-            '!' + config.js + '/libs/**/*.{js,jsx}',
-            '!' + config.js + '/bundle.js',
-            '!' + config.js + '/**/*.min.js'
-        ]);
-    });
-
     gulp.task('watch',<% if (useBrowserify) { %> ['browserify:vendor', 'browserify:app'],<% } %> function() {
         gulp.watch(config.dev + 'index.html', browserSync.reload);
         <% if (useBackbone) { %>gulp.watch(config.dev + 'templates/**/*.html', ['browserify:app']);<% } %>
@@ -48,7 +34,7 @@ if (argv.lint) {
             '!' + config.js + '/libs/**/*.{js,jsx}',<% if (useBrowserify) { %>
             '!' + config.js + '/bundle.js',<% } %>
             '!' + config.js + '/**/*.min.js'
-        ], ['lint']);
+        ]);
     });
 
 }
